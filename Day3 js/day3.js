359eaa006b6dc382390c5928b36ac5bae5d52a20
Shwetu-123 <144302422+Shwetu-123@users.noreply.github.com>
2023-10-06 16:30:51 +0530

    update

--- Day3 js/day3.js
+++ Day3 js/day3.js
@@ -1,14 +1,25 @@
-var age = 22
+var num1 = 11
+var num2 = 13
+var num3 = 18
+if(num1%2==0)
+{
+   if(num2%2==0)
+   {
+      console.log(`${num2} is even`)
+   }
+   else
+   {
+      console.log(`${num2} is odd`)
+   }
+}
+else
+{
+   if(num3%2==0)
+   {
+      console.log(`${num3} is even`)
+   }
+   else{
+      console.log(`${num3} is odd`)
+   }
 
- if(age>=18)
- {
-    console.log("You can vote")
-
- }
-
- else if (age<18)
- {
-    console.log("you can vote After " + 18-age +" Years")
- }
-
- 
+}
