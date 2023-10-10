d99b05a5f84619cbb44d4a7b21de9ff4460cc4db
Shwetu-123 <144302422+Shwetu-123@users.noreply.github.com>
2023-10-10 09:50:22 +0530

    updates

---
+++ Day5 js/Day5.js
@@ -0,0 +1,32 @@
+const Studentlist = ["Ram","Rahul","Priya"]
+{
+    console.log(Studentlist);
+
+}
+
+const list =[10,20,30,40,50]
+{
+    console.log(list)
+    console.log(list.length)
+
+}
+
+
+const list2 =[10,"Rahul",true,list]
+{
+    
+    console.log(list2)
+
+}
+const list3 =[10,"Rahul",true,list]
+{
+
+    console.log(list3[2])
+
+}
+
+const studentinfo = ["Suraj","Ram","Rahul","Priya"]
+{
+    for(var i=0;i<studentinfo.length;i++)
+    console.log(studentinfo[i])
+}
